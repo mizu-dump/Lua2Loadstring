9fd3db051cfee065a0fd72eb5cdad0d6f6da0f8f
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-11-06 21:50:19 +0800

    Uploaded via LuaToLoadstring

---
+++ Main/sisjjsjsjejeje.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://github.com/mizu-dump/Lua2Loadstring/raw/main/example.lua"))()
+
+loadstring(game:HttpGet("https://github.com/mizu-dump/Lua2Loadstring/raw/main/example.lua"))()jdjdjdd
+njsjsjsbs tdysvidbidbidvdh 
