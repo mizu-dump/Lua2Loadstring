c91ee7ed2be31331a565a51d412b048aadec5ab9
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-11-06 21:08:17 +0800

    Uploaded via LuaToLoadstring

---
+++ Main/khyrie_608.lua
@@ -0,0 +1,4 @@
+blox fruit
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TlDinhKhoi/Xeter/refs/heads/main()()true
+loadstring(game:HttpGet("https://github.com/mizu-dump/Lua2Loadstring/raw/main/example.lua"))()
+loadstring(game:HttpGet("https://github.com/mizu-dump/Lua2Loadstring/raw/main/example.lua"))()
