3412aab1a02501b40e7caf1aa18ef151127a13c9
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-11-07 00:01:36 +0800

    Uploaded via LuaToLoadstring

---
+++ Main/redz hab.lua
@@ -0,0 +1,7 @@
+
+local Settings = {
+    JoinTeam = "Pirates"; -- Pirates / Marines
+    Translator = true;   -- true / false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Scripts/refs/heads/main/main.luau"))(Settings)
