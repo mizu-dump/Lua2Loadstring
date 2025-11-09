2fdab76fbabc6dfb6f1a1d50795b389e4fc9b241
𝐌 𝐈 𝐙 𝐔 𝐌 𝐀 𝐑 𝐔 <mizumaruofficialdump@gmail.com>
2025-11-09 19:54:36 +0800

    Uploaded via LuaToLoadstring

---
+++ Main/skid.lua
@@ -0,0 +1,15 @@
+{
+  "messageCount": 1,
+  "exportTime": "2025-11-07 19:34:53",
+  "messages": [
+    {
+      "isUser": false,
+      "author": "Morfy",
+      "category": "default",
+      "id": 1,
+      "text": "Hello, I'm Morfy, your friendly AI assistant, here to help you navigate and get the most out of morfOS 11.2 Astrum.",
+      "isPinned": false,
+      "timestamp": "19:34"
+    }
+  ]
+}
